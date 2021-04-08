d73df1d35e697c3a39650c734c2498b14219172f
edwinHernandezB <edwinhb139@gmail.com>
2021-04-08 19:29:42 +0200

    Find devices done, working to add MAC address in tables result

--- Backend/main.js
+++ Backend/main.js
@@ -3,7 +3,7 @@ const execsync = require('child_process').execSync
 const cors = require('cors');
 const dns = require('dns');
 
-var exec = require('child_process').exec;
+let exec = require('child_process').exec;
 express = require("express"),
 app = express();
 app.use(cors());
@@ -68,36 +68,15 @@ app.get('/packetPath', function(req, res){
     
     if (req.query.option == 0) {
         console.log('dentro 0');
-       /* try {
-            let result = execsync('traceroute -m 20 -w 1 ' + req.query.value)
-            console.log(result + ' ');
-            res.send(result)
-            res.end()
-            
-        } catch (error) {
-            console.log(error + '');
-            res.end(`Error has ocurred: ${error}`)
-
-        }*/
-        /*exec('traceroute -m 20 -w 1 ' + req.query.value, (error, stdout, stderr) => {
-            if (error) {
-              res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-              console.error(`exec error: ${error}`);
-              res.end(`Error has ocurred: ${error}`)
-              return;
-            }else{ 
-                res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-                res.end(stdout)
-            }
-        })*/
+   
         str = "";
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
         const traceroute = spawn('traceroute',['-m', '20', '-w' ,'1', req.query.value] )
         traceroute.stdout.on('data', (data) => {
             str += data.toString();
 
-             var lines = str.split("\n");
-            for(var i in lines) {
+             let lines = str.split("\n");
+            for(let i in lines) {
             if(i == lines.length - 1) {
                 str = lines[i];
             } else{
@@ -142,8 +121,93 @@ app.get('/packetPath', function(req, res){
    
 
     });    
-        
-   
+
+
+function checkIp(ip) {
+    return new Promise(function (resolve, reject) {
+        let shellCmd = 'ping -c 1 -w 1 ' + ip + ' | grep ttl*';
+        exec(shellCmd, function (err, stdout, stderr) {
+            if (err) {
+                reject(err);
+            }
+            let result = [];
+            if(stdout.includes('64')){
+                result.push({ip: ip,active: 'Active Device'})
+            }
+            
+            resolve(result);
+        });
+    });
+}
+
+
+function resolveDomain(ip) {
+    return new Promise(function (resolve, reject) {
+        let shellCmd = 'nslookup ' + ip;
+        exec(shellCmd, function (err, stdout, stderr) {
+            
+            let result = [];
+            if (stdout.includes('**')) {
+                result.push({IP: ip, domain: 'Domain not found'});
+            }
+            else{
+                let res = stdout.toString();
+                res = stdout.split('name = ')
+                res = res[1].split('.\n')
+                result.push({IP: ip, domain: res[0]});
+            }
+            
+            resolve(result);
+        });
+    });
+}
+
+app.get('/scanCompleteNetwork', function(req, res){
+    console.log(req.query.ip);
+    console.log(req.query.range);
+
+    let ip = ''
+    let inicialRange = 1
+    let finalRange = req.query.range -1
+    //let range = req.query.range -1
+    let promises = [];
+    let handledIP = req.query.ip.split('.')
+    if (handledIP[3] == '0') {
+        console.log('es red');
+        ip = req.query.ip.slice(0,req.query.ip.length-1)
+        for (let i = inicialRange; i < finalRange ; i++) {
+            console.log(ip+i);
+            promises.push(checkIp(ip + i));
+        }
+    }else{
+        console.log('es un rango');
+        ip = req.query.ip.slice(0,req.query.ip.length - handledIP[3].length)
+        console.log(ip);
+        inicialRange = handledIP[3]
+        console.log(inicialRange);
+        finalRange = req.query.range -1
+        for (let i = inicialRange; i <= finalRange +1; i++) {
+            console.log(ip+i);
+            promises.push(checkIp(ip + i));
+        }
+    }
+    
+  
+    Promise.all(promises).then(function (results) {
+        let ipDomain = []
+        for (let i = 0; i < results.length; i++) {
+            if(results[i][0] != null)
+            {
+                ipDomain.push(resolveDomain(results[i][0].ip))
+            }
+        }
+        Promise.all(ipDomain).then(function (results) {
+            res.send(results)
+        })
+    });
+
+})
+  
 
 app.listen(4000);
 console.log('Servidor escuchando en puerto 4000');
--- src/views/functionalities/FindDevices.vue
+++ src/views/functionalities/FindDevices.vue
@@ -3,25 +3,143 @@
   <v-app-bar  app clipped-left elevation="0"  class="vAppBar" height="40" >
     <v-app-bar-title>Encontrar dispositivos connectados en la red</v-app-bar-title>
   </v-app-bar>
+    <v-row class="ml-1">
+      <!-- Select option -->
+      <v-col cols="12" md="4">
+        <v-select style="margin-top:20px;" 
+          :items="accions" label="Elige una opción"
+          solo v-model="activeAction" color="info">
+        </v-select>
+      </v-col>
+    </v-row>
+    <v-form >
+      <v-container fluid v-if="activeAction != ''">
+        <!--Form de IP-->
+        <v-row v-if="activeAction != ''">
+          <v-col  cols="12" md="3">
+            <v-text-field v-model="IP" :rules="activeAction == accions[0] ? rulesIPNetwork: rulesIP" 
+             :label="activeAction == accions[0] ? 'IP de red (p.e: 192.168.1.0)':'Escanear desde (p.e: 192.168.1.50)'" required></v-text-field>
+          </v-col>
+           <v-col v-if="activeAction != ''" cols="12" md="3">
+            <v-text-field v-model="mask" :rules="activeAction == accions[0] ? rulesMask: rulesIPFinal" 
+            type="number"
+            :label="activeAction == accions[0] ? 'Máscara (p.e entero del 24 hasta 32)':'Escanear hasta (p.e: 192.168.1.100)'" required>
+            </v-text-field>
+          </v-col>
+            <v-btn v-if="activeAction == accions[0]" color="success" class="mt-6 ml-5 " :disabled="!isCorrectIPNetwork" @click="ejecutarScanCompleto"> Ejecutar </v-btn>  
+            <v-btn v-if="activeAction == accions[1]" color="success" class="mt-6 ml-5 " :disabled="!isCorrectIP" @click="ejecutarScanCompleto"> Ejecutar </v-btn>  
+          </v-row>
+          <!-- Table -->   
+      <v-card >
+        <v-card-title>
+          Dispositivos encontrados 
+          <v-spacer></v-spacer>
+          <v-text-field 
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Buscador"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table 
+          :headers="headers"
+          :items="desserts"
+          :search="search"
+        ></v-data-table>
+      </v-card>
+      <!-- Alerta de cancelación de proceso -->
+      <v-alert :value="errorData" type="error"> Proceso cancelado.</v-alert>
+      </v-container>
+    </v-form>
 
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, et adipisci voluptate, quas sit, a quibusdam fugiat obcaecati voluptatum blanditiis unde. Magnam tempore quasi unde enim id ducimus modi fugiat! </h1>
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, et adipisci voluptate, quas sit, a quibusdam fugiat obcaecati voluptatum blanditiis unde. Magnam tempore quasi unde enim id ducimus modi fugiat! </h1>
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, et adipisci voluptate, quas sit, a quibusdam fugiat obcaecati voluptatum blanditiis unde. Magnam tempore quasi unde enim id ducimus modi fugiat! </h1>
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, et adipisci voluptate, quas sit, a quibusdam fugiat obcaecati voluptatum blanditiis unde. Magnam tempore quasi unde enim id ducimus modi fugiat! </h1>
-  <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, et adipisci voluptate, quas sit, a quibusdam fugiat obcaecati voluptatum blanditiis unde. Magnam tempore quasi unde enim id ducimus modi fugiat! </h1>
- 
 </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios'
 
 export default {
   name: 'Prueba',
   components: {
     
+  },
+  data() {
+    return {
+      accions: ['Escanear la red', 'Escanear red solo a partir de un rango IP'],
+      activeAction: '',
+      IP: '192.168.1.0',
+      mask: 0,
+      errorData: false,
+      //---------- Table ---------------
+       search: '',
+       headers: [
+      {
+        text: 'IP',
+        align: 'center',
+        sortable: false,
+        value: 'ip',
+       },
+       { text: 'Domain', value: 'domain' },
+
+      ],
+      desserts: [],
+      
+       //----------- Rules form --------------
+      //-------------- Option Scan network ----------------
+      isCorrectIPNetwork: false,
+      isCorrectIP: false,
+      rulesIPNetwork:[
+         v => !!v || 'IP de red es requerida',
+         v => /^(?:[0-9]{1,3}\.){3}[0]{1}$/.test(v) || 'Formato de IP incorrecta',
+         v => { if(/^(?:[0-9]{1,3}\.){3}[0]{1}$/.test(v)){ this.isCorrectIPNetwork = true}else { this.isCorrectIPNetwork = false} return true }
+      ],
+        rulesMask: [
+        v => {if(v < 24){this.mask = 24} return true},
+        v => {if(v > 32){this.mask = 32} return true},
+      ],
+      //-------------- Option Scan range of the network ----------------
+      rulesIP:[
+         v => !!v || 'IP es requerida',
+         v => /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(v) || 'Formato de IP incorrecto',
+         v => { if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(v)){ this.isCorrectIP = true}else { this.isCorrectIP = false} return true }
+      ],
+      rulesIPFinal:[
+        v => !!v || 'IP es requerida',
+        v => {if(v < 1){this.mask= 1} return true},
+        v => {if(v > 256){this.mask = 256} return true},
+      ],
+    }
+  },
+  methods: {
+    ejecutarScanCompleto: function(){
+        let params = { ip: this.IP, 
+                      range: this.activeAction == this.accions[0] ? 2**(32 - this.mask):this.mask}
+        this.desserts = []
+        axios
+        .get('http://localhost:4000/scanCompleteNetwork', { params })
+        .then(response =>{
+          console.log(response.data);
+          for (let index = 0; index < response.data.length; index++) {
+            this.desserts.push({
+              ip: response.data[index][0].IP,
+              domain: response.data[index][0].domain
+            })  
+          }
+          
+        })
+    },
+    ejecutarScanParcial: function(){
+
+    }
+  },
+  watch:{
+    activeAction: function(){
+      this.IP = ''
+    }
   }
 }
+
 </script>
 
 <style scoped>
